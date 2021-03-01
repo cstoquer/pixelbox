33e6d3aad5e06a902a4845cb3d12b252c207e070
Cedric Stoquer <cstoquer@gmail.com>
2021-03-01 20:42:07 +0900

    Set MAG_FILTER to nearest for all textures

--- Texture/webGL.js
+++ Texture/webGL.js
@@ -23,6 +23,7 @@ Texture.prototype._init = function () {
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.width, this.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
 	// set the filtering so we don't need mips
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
